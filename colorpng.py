7f2ce6be7889a5845c1bc4c74f93c2819da7f7ca
haruto6691 <haruto.m.782153@gmail.com>
2024-11-10 21:27:20 +0900

    ka

--- colorpng.py
+++ colorpng.py
@@ -4,11 +4,11 @@ import numpy as np
 # 画像の読み込み
 img = cv2.imread('img.png')
 
-# 指定色 (白色)
+# 指定色 (黄色)
 target_color = (255, 255, 255)
 
 # 変更後の色 (例: 黒色)
-change_color = (255, 160, 150)
+change_color = (101, 150, 100)
 
 # 色の変更
 # 各ピクセルが指定された色と一致する場合、その色を変更
