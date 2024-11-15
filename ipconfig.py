48dcc87743ff952f579e68fe4d1082747d533dcf
haruto6691 <haruto.m.782153@gmail.com>
2024-11-15 13:33:30 +0900

    a

--- ipconfig.py
+++ ipconfig.py
@@ -1,6 +1,8 @@
 import requests
+import streamlit as st
 
 # ipify APIを使ってグローバルIPアドレスを取得
 ip_address = requests.get('https://api.ipify.org').text
 
-print(f"Your global IP address is: {ip_address}")
+# print(f"Your global IP address is: {ip_address}")
+st.title(f"Your global IP address is: {ip_address}")
