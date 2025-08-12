11e634deef242d89f1b308c0498bad1c3461e55d
haruto6691 <haruto.m.782153@gmail.com>
2025-08-12 16:30:59 +0900

    aborts

--- mapdata/ipconfig.py
+++
@@ -1,7 +0,0 @@
-import requests
-import streamlit as st
-
-# ipify APIを使ってグローバルIPアドレスを取得
-ip_address = requests.get('https://api.ipify.org').text
-
-st.write(f"Your global IP address is: {ip_address}")
